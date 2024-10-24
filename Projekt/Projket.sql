772e8091e45d56a1e40be345555c3fb85dec1763
platteXDlol <Pascal_Hocher@sluz.ch>
2024-10-24 13:00:54 +0200

    table Studenten without PK, FK

--- Projekt/Projket.sql
+++ Projekt/Projket.sql
@@ -3,5 +3,12 @@ GO
 
 USE Projekt;
 
-CREATE TABLE 
+CREATE TABLE Studenten (
+    Matrikelnummer INT NOT NULL
+    Vorname nvarchar(20) NOT NULL,
+    Nachname nvarchar(20) NOT NULL,
+    Studienbeginn DATABASE NOT NULL,
+    Vorlesung INT NOT NULL,
+    Prüfung INT NOT NULL, 
+)
 
